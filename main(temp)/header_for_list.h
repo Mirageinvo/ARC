7a4c54f238ab45c02e4310df1c9b7c753f2a231c
includealex <70340829+includealex@users.noreply.github.com>
2021-05-13 21:10:42 +0300

    Update header_for_list.h

--- main(temp)/header_for_list.h
+++ main(temp)/header_for_list.h
@@ -1,1 +1,13 @@
+//LIST (not a paper)
 
+struct node {
+  int* next;
+  int* prev;
+  int data;
+  int val;
+}
+
+add_to_list(int data, int* adress) {};
+delet_from_list(int data, int* adress) {};
+length_of(int *adress) {};
+remove_from_cache(int *adress) {};
