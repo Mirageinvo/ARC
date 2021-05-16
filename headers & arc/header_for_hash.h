2db26f5e36c8d2303ac405f088ccd92530075578
includealex <70340829+includealex@users.noreply.github.com>
2021-05-16 15:57:18 +0300

    Update header_for_hash.h

--- headers & arc/header_for_hash.h
+++ headers & arc/header_for_hash.h
@@ -1,4 +1,11 @@
 //HASH
-int change_val_to_adr(int page) {};
-int* create_hash (int page) {};
-int* adr_of_page_hash (int page) {};
+
+//stdin  -> page
+//stdout -> adress of that page + in that adress should be val of that page + I should fill
+//          the hash_table
+
+void create_hash_table(struct node** hash_table);
+int hash_for_idx(int page);
+struct node* check(struct node* page, struct node** hash_table);
+void nulify(struct node* page, struct node** hash_table);
+void create_el (int page_val, struct node** hash_table);
