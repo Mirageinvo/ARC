220ccac28f81f2364473093f50e5784d413ce4ca
Mirageinvo <70330507+Mirageinvo@users.noreply.github.com>
2021-05-17 03:27:13 +0300

    Update header_for_hash.h

--- source/header_for_hash.h
+++ source/header_for_hash.h
@@ -1,6 +1,6 @@
 //HASH
 
-struct node ** create_hash_table(struct node** hash_table);
+struct node ** create_hash_table();
 int hash_for_idx(int page);
 struct node* check(int page, struct node** hash_table);
 void nulify(struct node* page, struct node** hash_table);
