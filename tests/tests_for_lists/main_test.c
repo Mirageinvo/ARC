d1345494a03ab4e2017356e54f1f6843c2466ed1
Mirageinvo <70330507+Mirageinvo@users.noreply.github.com>
2021-05-17 14:29:45 +0300

    Add files via upload

---
+++ tests/tests_for_lists/main_test.c
@@ -0,0 +1,13 @@
+//needed to include main_test.c, list_test.c, arc_list.c
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "list_test_header.h"
+
+int main() {
+
+	test_for_list();
+	printf("\nAll tests for list have been passed\n");
+
+	return 0;
+}
