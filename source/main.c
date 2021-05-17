e880299f436e72f8a7f540dcdff85eef4c7bae0f
Mirageinvo <70330507+Mirageinvo@users.noreply.github.com>
2021-05-17 03:24:43 +0300

    Update main.c

--- source/main.c
+++ source/main.c
@@ -8,7 +8,7 @@ int main () {
     int number_of_hits;
     struct node ** hash_table;
     number_of_hits = 0;
-    hash_table = create_hash_table(hash_table);
+    hash_table = create_hash_table();
     number_of_hits = ARC(hash_table);
     
     printf("number of hits to hash: %d\n", number_of_hits);
